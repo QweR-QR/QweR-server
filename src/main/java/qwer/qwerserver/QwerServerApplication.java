c47b723291b384e80e9d94397a4c60d5ddf27ed0
kangCoder <khsofficial1213@gmail.com>
2023-02-20 05:46:12 +0900

    Feat: initial commit

---
+++ src/main/java/qwer/qwerserver/QwerServerApplication.java
@@ -0,0 +1,13 @@
+package qwer.qwerserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QwerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QwerServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/qwer/qwerserver/QwerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package qwer.qwerserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QwerServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
